ac0fab58b009b4d00c457cbf09fe87600da4ff3f
Rafsyanjani43 <rafsyanjani43@gmail.com>
2023-10-30 08:32:29 +0700

    Praktikum Modul33333

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /**
+     * mengentod harus pakai kondom
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
